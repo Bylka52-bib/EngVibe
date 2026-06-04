381d662fe3b75695726b2a1fb31e381935649a41
Plyaskina Ekaterina <plaskinaekaterina2007@gmail.com>
2026-06-05 01:53:54 +0900

    impotr os

--- EngVibe/settings.py
+++ EngVibe/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-0-(y6t1-v7ra63g#!923^=m9zixzhu47i!37wp*%=8le=uc7f8'
 
+import os
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['*']
